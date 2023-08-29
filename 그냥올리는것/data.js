b3d43d308d2566504dac4b382b2a2865c8770abd
minsub12 <143483667+minsub12@users.noreply.github.com>
2023-08-29 17:22:00 +0900

    comit

---
+++ 그냥올리는것/data.js
@@ -0,0 +1,80 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-img_20230829_141446_00_003",
+      "name": "IMG_20230829_141446_00_003",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -2.4331248697337795,
+        "pitch": 0.049619886452095585,
+        "fov": 1.2590238327822512
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.9028395407214944,
+          "pitch": 0.5118595053253934,
+          "rotation": 2.356194490192345,
+          "target": "1-img_20230829_141446_00_003"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-img_20230829_141446_00_003",
+      "name": "IMG_20230829_141446_00_003",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": true
+  }
+};
