9fe040350c014fbca4b7a327d72c0f294f97d5d4
minsub12 <143483667+minsub12@users.noreply.github.com>
2023-08-30 10:33:20 +0900

    comuit

---
+++ app-files/data.js
@@ -0,0 +1,552 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "두리함께건물외부",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.47664030958097214,
+          "pitch": 0.15324318688693594,
+          "rotation": 6.283185307179586,
+          "target": "2-11"
+        },
+        {
+          "yaw": -1.1951680418199526,
+          "pitch": 0.10394826441015148,
+          "rotation": 0,
+          "target": "7-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.4954576920726037,
+          "pitch": -0.5064080692513571,
+          "title": "출입구",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "1-1",
+      "name": "1층계단및남자화장실",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.0852429618281505,
+          "pitch": 0.6745037949364985,
+          "rotation": 0,
+          "target": "2-11"
+        },
+        {
+          "yaw": 2.3023840209245394,
+          "pitch": 0.7391980735254222,
+          "rotation": 5.497787143782138,
+          "target": "5-2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.376669257220689,
+          "pitch": -0.06640165179665658,
+          "title": "남자화장실",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "2-11",
+      "name": "1층내부(1)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.0257826056626174,
+          "pitch": 0.3287007738487926,
+          "rotation": 6.283185307179586,
+          "target": "0-"
+        },
+        {
+          "yaw": -2.8737924785013433,
+          "pitch": 0.23984417089754473,
+          "rotation": 0,
+          "target": "7-"
+        },
+        {
+          "yaw": -1.6024407580604496,
+          "pitch": 0.4922605050368105,
+          "rotation": 0,
+          "target": "1-1"
+        },
+        {
+          "yaw": -0.6213891342617579,
+          "pitch": 0.46963519819189337,
+          "rotation": 5.497787143782138,
+          "target": "4-1"
+        },
+        {
+          "yaw": 0.012544581971472013,
+          "pitch": 0.20338540636795344,
+          "rotation": 0,
+          "target": "3-12"
+        },
+        {
+          "yaw": -1.5961960508876007,
+          "pitch": -0.016754601464079144,
+          "rotation": 7.853981633974483,
+          "target": "8-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.7824625094863595,
+          "pitch": -0.0917263025847248,
+          "title": "김아름",
+          "text": "사무국장"
+        },
+        {
+          "yaw": -0.8376205887828423,
+          "pitch": -0.25870771737907106,
+          "title": "장애인화장실",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "3-12",
+      "name": "1층내부(2)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0898202043246226,
+          "pitch": 0.4979757373527942,
+          "rotation": 0,
+          "target": "2-11"
+        },
+        {
+          "yaw": -1.2268229810725515,
+          "pitch": 0.30345478926868275,
+          "rotation": 0,
+          "target": "8-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.7550664615815528,
+          "pitch": -0.1548582281224089,
+          "title": "손창신",
+          "text": "차장"
+        },
+        {
+          "yaw": 1.1634566907100137,
+          "pitch": -0.13174468588736588,
+          "title": "오주영",
+          "text": "사원"
+        },
+        {
+          "yaw": 0.544029906131005,
+          "pitch": -0.1505384397399787,
+          "title": "김예진",
+          "text": "사원"
+        },
+        {
+          "yaw": 2.321703636578606,
+          "pitch": -0.05670590580137613,
+          "title": "홍준영",
+          "text": "사원"
+        }
+      ]
+    },
+    {
+      "id": "4-1",
+      "name": "1층장애인화장실",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.857549041582674,
+          "pitch": 0.760140311318267,
+          "rotation": 0,
+          "target": "3-12"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-2",
+      "name": "2층내부",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.05821644816879079,
+          "pitch": 0.01205131125617065,
+          "rotation": 0,
+          "target": "6-2"
+        },
+        {
+          "yaw": -1.676859889354347,
+          "pitch": 0.615366525040681,
+          "rotation": 0,
+          "target": "1-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.44589549057954514,
+          "pitch": -0.15473855444430917,
+          "title": "신화단",
+          "text": "매니저"
+        },
+        {
+          "yaw": 0.8394911200904716,
+          "pitch": -0.1883998296906384,
+          "title": "전혜민",
+          "text": "차장"
+        },
+        {
+          "yaw": 2.035993773282785,
+          "pitch": -0.08510119300991548,
+          "title": "김경란",
+          "text": "팀장"
+        },
+        {
+          "yaw": 2.8593039523764565,
+          "pitch": -0.03656914928121857,
+          "title": "박효성",
+          "text": "부장"
+        },
+        {
+          "yaw": -3.010096697693923,
+          "pitch": -0.07258510399144313,
+          "title": "함준호",
+          "text": "차장"
+        },
+        {
+          "yaw": -2.8524261009015213,
+          "pitch": -0.05593283163626417,
+          "title": "정진덕",
+          "text": "팀장"
+        },
+        {
+          "yaw": -0.7915539478477918,
+          "pitch": -0.22699707192841956,
+          "title": "여자화장실+",
+          "text": ""
+        },
+        {
+          "yaw": 0.2593761263192995,
+          "pitch": -0.1386101466098335,
+          "title": "이보교",
+          "text": "대표"
+        }
+      ]
+    },
+    {
+      "id": "6-2",
+      "name": "2층회의실",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.7515193592847318,
+          "pitch": 0.6628963397485457,
+          "rotation": 0,
+          "target": "5-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-",
+      "name": "두리함께주차장",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5348164074665185,
+          "pitch": 0.30399404515578965,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": -1.202306439708563,
+          "pitch": 0.17698204638690385,
+          "rotation": 0,
+          "target": "8-"
+        },
+        {
+          "yaw": -0.36343871910946035,
+          "pitch": 0.5696207578055947,
+          "rotation": 12.566370614359176,
+          "target": "2-11"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.28568595627258375,
+          "pitch": -0.15665095239285876,
+          "title": "자동문",
+          "text": "휠체어 이용인들을 위한 자동문"
+        },
+        {
+          "yaw": 0.7366865142182863,
+          "pitch": 1.318622709639948,
+          "title": "주차장",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "8-",
+      "name": "두리함께테라스",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.346551449479641,
+          "pitch": 0.7123816397915199,
+          "rotation": 0,
+          "target": "2-11"
+        },
+        {
+          "yaw": -0.10408877469639854,
+          "pitch": -0.04874939410908219,
+          "rotation": 7.853981633974483,
+          "target": "3-12"
+        },
+        {
+          "yaw": 2.965073841345861,
+          "pitch": 0.09378862292366819,
+          "rotation": 4.71238898038469,
+          "target": "7-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.37064620029129713,
+          "pitch": -0.11544511901739263,
+          "title": "직원휴게 테라스",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
